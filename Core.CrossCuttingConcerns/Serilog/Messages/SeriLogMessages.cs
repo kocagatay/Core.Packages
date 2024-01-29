5bf0f3eba48f2d00967b623ee8beb58a44e7f91d
Çağatay Koç <kocagatayy@gmail.com>
2024-01-30 00:41:31 +0300

    Add project files.

---
+++ Core.CrossCuttingConcerns/Serilog/Messages/SeriLogMessages.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.CrossCuttingConcerns.Serilog.Messages;
+
+public static class SeriLogMessages
+{
+    public static string NullOptionsMessage => "you have sent a blank value! Something went wrong. Please try again.";
+}
